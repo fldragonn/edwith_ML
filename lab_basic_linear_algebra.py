4fc2f01d8ba13eaa74ccfb3d62bd3624c1e0fbe6
jeondaelyong <fldragonjun@gmail.com>
2020-04-08 16:59:21 +0900

    quiz1. 완료

--- lab_basic_linear_algebra.py
+++ lab_basic_linear_algebra.py
@@ -10,49 +10,58 @@ def vector_addition(*vector_variables):
 def vector_subtraction(*vector_variables):
     if vector_size_check(*vector_variables) == False:
         raise ArithmeticError
-    return None
 
+    result = [row[0] - sum(row[1:]) for row in zip(*vector_variables)]
+    return result
 
 def scalar_vector_product(alpha, vector_variable):
-    return None
+    result = [alpha * row for row in vector_variable]
+    return result
 
 
 def matrix_size_check(*matrix_variables):
-    return None
+    result = all([len(set(len(matrix[0]) for matrix in matrix_variables))==1]) and all([len(matrix_variables[0])==len(matrix) for matrix in matrix_variables])
+    return result
 
 
 def is_matrix_equal(*matrix_variables):
-    return None
+    result = all([ all([len(set(row)) == 1 for row in zip(*matrix)]) for matrix in zip(*matrix_variables)])
+    return result
 
 
 def matrix_addition(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    result = [[sum(row) for row in zip(*matrix)] for matrix in zip(*matrix_variables)]
+    return result
 
 
 def matrix_subtraction(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    result = [[row[0]*2 - sum(row) for row in zip(*matrix)] for matrix in zip(*matrix_variables)]
+    return result
 
 
-def matrix_transpose(matrix_variable):
-    return None
+def matrix_transpose(*matrix_variable):
+    return [ [element for element in row] for row in zip(*matrix_variable)]
 
 
 def scalar_matrix_product(alpha, matrix_variable):
-    return None
+    return [scalar_vector_product(alpha, row) for row in matrix_variable]
 
 
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    return len([column_vector for column_vector in zip(*matrix_a)]) == len(matrix_b)
 
 
 def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
-    return None
+
+    return [ [sum(a * b for a, b in zip(row_a, column_b))
+              for column_b in zip(*matrix_b)]
+             for row_a in matrix_a]
 
 # 실행결과
 print(vector_size_check([1,2,3], [2,3,4], [5,6,7])) # Expected value: True
@@ -108,7 +117,7 @@ print (matrix_subtraction(matrix_x, matrix_y, matrix_z)) # Expected value: [[-2,
 
 # 실행결과
 matrix_w = [[2, 5], [1, 1], [2, 2]]
-matrix_transpose(matrix_w)
+print(matrix_transpose(matrix_w))
 
 # 실행결과
 matrix_x = [[2, 2], [2, 2], [2, 2]]
